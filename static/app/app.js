cd63b02ffe222cdb0f9567b634ba4f220d7fa13e
Md-Mosabbir <kmosabbir@gmail.com>
2023-05-20 19:57:20 +0600

    class added successfully and now editing part

--- static/app/app.js
+++ static/app/app.js
@@ -15,11 +15,13 @@ AddingNewBook.addEventListener('click', () => {
 })
 // --
 
-function Book (title, author, pages, read) {
-  this.title = title
-  this.author = author
-  this.pages = pages
-  this.read = read
+class Book {
+  constructor (title, author, pages, read) {
+    this.title = title
+    this.author = author
+    this.pages = pages
+    this.read = read
+  }
 }
 // Displays Book
 
